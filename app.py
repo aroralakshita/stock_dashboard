be7fe67844f83fccbb74f85f80195adc66e094fd
aroralakshita <alakshita07@gmail.com>
2025-11-15 20:46:30 +0100

    streamlit integration - plot_closing_price_with_ma

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
-from src.data_processing import clean_numeric_columns, moving_avg
-from src.fetch_data import load_csv
+import streamlit as st
+import pandas as pd
+from src.data_processing import load_csv, clean_numeric_columns, moving_avg
 from src.viz import plot_closing_price_with_ma
 
-df = load_csv("data/AAPL.csv")
+st.sidebar.title("stock dashboard")
+
+ticker = st.sidebar.selectbox("select stock", ["AAPL"])
+
+st.write("DEBUG — ticker Streamlit received:", ticker)
+print("DEBUG — ticker:", ticker)
+
+
+ma_options = st.sidebar.multiselect(
+    "Select Moving Averages",
+    options=[20, 50],
+    default=[20, 50]
+)
+
+start_date = st.sidebar.date_input("Start Date", value=pd.to_datetime("2020-11-16"))
+end_date = st.sidebar.date_input("End Date", value=pd.to_datetime("2025-11-14"))
+
+df = load_csv(ticker)
 df = clean_numeric_columns(df)
+
+df = df[(df["date"] >= pd.to_datetime(start_date)) & (df["date"] <= pd.to_datetime(end_date))]
+
+df = moving_avg(df, windows=ma_options)
 
 print(df.head())
 print(df.info())
-
-
-df = moving_avg(df, windows=[20, 50])
-
 print(df.columns)
 
 fig = plot_closing_price_with_ma(df)
-fig.show()
+st.plotly_chart(fig, use_container_width=True)
+
+if st.checkbox("Show Raw Data"):
+    st.dataframe(df)
 
 
