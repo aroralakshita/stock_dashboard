f7a7ee7e43e9d230e2d4c52874986a2b8a5ae4c7
aroralakshita <alakshita07@gmail.com>
2025-11-15 20:43:20 +0100

    use csv for protype

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,26 +1,38 @@
 import yfinance as yf
 import pandas as pd
 
-def fetch_stock(ticker, start_date, end_date):
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#def fetch_stock(ticker, start_date, end_date):
  # ticker → stock symbol (e.g., "AAPL" for Apple)
     
-   stock = yf.Ticker(ticker)
-   df = stock.history(start=start_date, end=end_date)
+   #stock = yf.Ticker(ticker)
+   #df = stock.history(start=start_date, end=end_date)
     
-   if df.empty:
-      raise ValueError(f"No data returned for {ticker}. Try again in 30–60 seconds.")
+   #if df.empty:
+      #raise ValueError(f"No data returned for {ticker}. Try again in 30–60 seconds.")
 
-   df = df.reset_index()
-   df.columns = [str(c).lower().replace(" ", "_") for c in df.columns]
 
-   return df
+def load_csv(ticker: str):
+    path = os.path.join(BASE_DIR, "data", f"{ticker}.csv")
+    print("Loading:", path)
+    df = pd.read_csv(path)
+    df = df.reset_index()
+    df.columns = [str(c).lower().replace(" ", "_") for c in df.columns]
+    df["date"] = pd.to_datetime(df["date"])
+
+    return df
+
 
 def clean_numeric_columns(df):
 
-   price_cols = ["close/last", "open", "high", "low"]
+   df = df.rename(columns={"close/last": "close"})
+
+   price_cols = ["close", "open", "high", "low"]
    for col in price_cols:
       df[col] = df[col].astype(str).str.replace("$", "", regex=False).str.replace(",", "", regex=False).astype(float)
-      df = df.rename(columns={"close/last": "close"})
 
    return df
 
@@ -29,3 +41,8 @@ def moving_avg(df, windows=[20, 50]):
       col_name = f"ma_{w}"
       df[col_name] = df["close"].rolling(window=w).mean()
    return df
+
+if __name__ == "__main__":
+    print("TEST: data_processing.py is running")
+    df = load_csv("AAPL")
+    print(df.head())
