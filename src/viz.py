badea51cbf65ae46986fe5b7649488adf2740999
aroralakshita <alakshita07@gmail.com>
2025-11-15 18:49:45 +0100

    add function to plot closing price with ma

--- src/viz.py
+++ src/viz.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import plotly.graph_objects as go
 
 def plot_closing_price(df):
 
@@ -15,6 +16,39 @@ def plot_closing_price(df):
         yaxis_title="Closing Price ($)",
         template="plotly_white"
     )
+    return fig
+
+def plot_closing_price_with_ma(df, title="Closing proce with moving averages"):
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(
+            x=df["date"],
+            y=df["close"],
+            mode="lines",
+            name="Close"
+        )
+    )
+
+    for c in df.columns:
+        if c.startswith("ma_"):
+            fig.add_trace(
+                go.Scatter(
+                    x=df["date"][df[c].notna()],
+                    y=df[c][df[c].notna()],
+                    mode="lines",
+                    name=c.upper()
+                )
+            )
+
+    fig.update_layout(
+        title=title,
+        xaxis_title="Date",
+        yaxis_title="Price ($)",
+        template="plotly_white"
+    )
+
     return fig
 
 
